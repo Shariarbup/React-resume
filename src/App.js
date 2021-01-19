a49d1e167d6af30ad45fc97f2b60e75714d36461
Al Shariar <itmasjoy@gmail.com>
2021-01-19 12:17:03 +0600

    resume

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import { Redirect, Route, Switch } from 'react-router-dom';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '../node_modules/bootstrap/dist/js/bootstrap.bundle';
+import About from './About';
+import './App.css';
+import Contact from './Contact';
+import Home from './Home';
+import Navbar from './Navbar';
+import Service from './Service';
+
+function App() {
+  return (
+    <>
+    <Navbar/>
+    <Switch>
+      <Route exact path='/' component={Home}/>
+      <Route exact path='/about' component={About}/>
+      <Route exact path='/contact' component={Contact}/>
+      <Route exact path='/service' component={Service}/>
+      <Redirect to="/" />
+      
+      
+    </Switch>
+    </>
+  );
+}
+
+export default App;
